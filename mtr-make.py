fb7703b14fcbd78acedb890676bb52f9037fddaf
BabeL <tongtianta4444@gmail.com>
2024-08-13 20:38:32 +0800

    change the way to compile to py

---
+++ mtr-make.py
@@ -0,0 +1,17 @@
+import os, sys, re, subprocess
+
+with open("chinaset-mtraddon.pnml","r",encoding='utf-8') as f:
+    files_content = f.read()
+    file_list = re.findall(r'#include\s+"([^"]+)"', files_content, re.MULTILINE)
+
+
+with open("chinaset-mtraddon.nml","w",encoding='utf-8') as f:
+    for file_name in file_list:
+        with open(file_name,"r",encoding='utf-8') as g:
+            files_content = g.read()
+        f.write(files_content)
+        f.write("\n")
+
+
+result = subprocess.run("nmlc chinaset-mtraddon.nml", shell=True, capture_output=True, text=True)
+print(result.stdout)
